0b9ef5a1b840966440668cbbdfe3886fe17785b4
Ash Rahman <asrahman97@gmail.com>
2020-02-11 14:30:07 -0500

    adding to seed

--- recipe_rest/db/seeds.rb
+++ recipe_rest/db/seeds.rb
@@ -16,12 +16,18 @@ user4 = User.create(user: "Rico Suave", password: "oi mate", image_url: "https:/
 
 user5 = User.create(user: "King Henry", password: "eminem", image_url: "https://media-exp1.licdn.com/dms/image/C4D03AQG9XANXACNiBA/profile-displayphoto-shrink_800_800/0?e=1586995200&v=beta&t=0zzQEQZrTl9ZQN-dQRY0jve3Te-lrEJdyXDCR_Q2vr4")
 
-recipe1 = Recipe.create(recipe_name: "Easiest Lasagna", rating: "", difficulty_level: 1, prep_time: 5, cook_time: 20, total_time: 40, image_url: "", video_url:_"")
+recipe1 = Recipe.create(recipe_name: "Easiest Lasagna", rating: 4, difficulty_level: 3, prep_time: 5, cook_time: 40, total_time: 45, image_url: "https://www.thewholesomedish.com/wp-content/uploads/2018/07/Best-Lasagna-550.jpg", video_url: "https://www.youtube.com/watch?v=jMq8lEu-of0")
 
-recipe2 = Recipe.create(recipe_name: "Cauliflower Chowder", rating: "", difficulty_level: 1, prep_time: 5, cook_time: 20, total_time: 40, image_url: "", video_url:_"")
+recipe2 = Recipe.create(recipe_name: "Cauliflower Chowder", rating: 4, difficulty_level: 2, prep_time: 5, cook_time: 60, total_time: 65, image_url: "https://www.twopeasandtheirpod.com/wp-content/uploads/2016/01/Creamy-Cauliflower-Chowder-3-500x375.jpg", video_url: "https://www.youtube.com/watch?v=TKtul4KDCKo")
 
-recipe3 = Recipe.create(recipe_name: "Lemon Butter Chicken", rating: "", difficulty_level: 1, prep_time: 5, cook_time: 20, total_time: 40, image_url: "", video_url:_"")
+recipe3 = Recipe.create(recipe_name: "Lemon Butter Chicken", rating: 4, difficulty_level: 1, prep_time: 10, cook_time: 30, total_time: 40, image_url: "https://www.saltandlavender.com/wp-content/uploads/2018/11/lemon-butter-chicken-recipe-2-500x500.jpg", video_url: "https://www.youtube.com/watch?v=Y27Yp_1hufk")
 
-recipe4 = Recipe.create(recipe_name: "Crockpot Bacon Potatoes", rating: "", difficulty_level: 1, prep_time: 5, cook_time: 20, total_time: 40, image_url: "", video_url:_"")
+recipe4 = Recipe.create(recipe_name: "Crockpot Bacon Potatoes", rating: 3, difficulty_level: 2, prep_time: 5, cook_time: 60, total_time: 65, image_url: "https://www.aforkstale.com/wp-content/uploads/2018/03/easy-crock-pot-cheesy-bacon-ranch-potatoes-3-500x500.jpg", video_url: "https://www.youtube.com/watch?v=VH6nmipIs3s")
 
-recipe5 = Recipe.create(recipe_name: "Slow Cooker Honey Garlic Chicken", rating: "", difficulty_level: 1, prep_time: 5, cook_time: 20, total_time: 40, image_url: "", video_url:_"")
+recipe5 = Recipe.create(recipe_name: "Slow Cooker Honey Garlic Chicken", rating: 5, difficulty_level: 2, prep_time: 5, cook_time: 200, total_time: 205, image_url: "https://www.spendwithpennies.com/wp-content/uploads/2013/10/Slow-Cooker-Honey-Garlic-Chicken-26-500x500.jpg", video_url: "https://www.youtube.com/watch?v=rsBeiP0BAHc")
+
+ingredient1 = Ingredient.create(ingredient_name: "Salt to taste")
+ingredient2 = Ingredient.create(ingredient_name: "Pepper to taste")
+ingredient3 = Ingredient.create(ingredient_name: "Broth")
+ingredient4 = Ingredient.create(ingredient_name: "Cumin")
+ingredient5 = Ingredient.create(ingredient_name: "Paprika")
