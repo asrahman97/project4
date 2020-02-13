7be68998a259d2fef3e75554851e57b746a292e2
Ash Rahman <asrahman97@gmail.com>
2020-02-13 10:47:34 -0500

    able to render data appjs

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
-import { registerUser, loginUser, verifyUser } from "./services/api_helper";
+import {
+  registerUser,
+  loginUser,
+  verifyUser,
+  recipeSingle,
+  recipesAll
+} from "./services/api_helper";
 import { Route, Link, withRouter } from "react-router-dom";
 import "./App.css";
 import Login from "./components/login";
-import Register from './components/register'
-import Navigation from './components/navigation'
+import Register from "./components/register";
+import Navigation from "./components/navigation";
+import RecipeGallery from "./components/recipeGallery";
 
 class App extends Component {
   constructor(props) {
@@ -26,88 +33,97 @@ class App extends Component {
     const currentUser = await registerUser(registerData);
     // if (!currentUser.errorMessage) {
     // console.log(currentUser)
-      this.setState({ currentUser });
-      // this.props.history.push('/recipes');
+    this.setState({ currentUser });
+    // this.props.history.push('/recipes');
     // } else {
     //   this.setState({ errorText: currentUser.errorMessage })
     // }
     // console.log("hey")
-  }
+  };
 
   handleLogin = async (e, loginData) => {
     e.preventDefault();
     const currentUser = await loginUser(loginData);
-    console.log(currentUser)
+    console.log(currentUser);
     this.setState({ currentUser });
     // this.props.history.push("/recipes");
-  }
+  };
 
   handleLogout = () => {
     this.setState({
       currentUser: null
-    })
-    localStorage.removeItem('authToken');
-    localStorage.removeItem('user');
-  }
+    });
+    localStorage.removeItem("authToken");
+    localStorage.removeItem("user");
+  };
 
-  getRecipe = async (id) => {
-    const recipe = await recipeSingle(id)
+  getRecipe = async id => {
+    const recipe = await recipeSingle(id);
     this.setState({
       recipe
-    })
-  }
+    });
+  };
 
   getRecipes = async () => {
-    const recipes = await recipesAll()
+    const recipes = await recipesAll();
     this.setState({
       recipes
-    })
-  }
-
-
+    });
+  };
 
   componentDidMount() {
     verifyUser();
-    if (localStorage.getItem('authToken')) {
-      const user = localStorage.getItem('user');
+    this.getRecipes()
+    if (localStorage.getItem("authToken")) {
+      const user = localStorage.getItem("user");
       const user1 = { user };
-      user && this.setState({
-        currentUser: user1
-      })
+      user &&
+        this.setState({
+          currentUser: user1
+        });
     }
   }
 
-
   render() {
-    console.log(this.state.currentUser)
+    console.log(this.state.currentUser);
     return (
       <div className="App">
-
-{ this.state.currentUser ?
+        {this.state.currentUser ? (
           <div>
             <h1>Hello, {this.state.currentUser.user}</h1>
             <button onClick={this.handleLogout}>Logout!!</button>
           </div>
-          :
-            <nav>
-              <Link to="/register"><button>Register</button></Link>
-              <Link to="/login"><button>Login</button></Link>
-            </nav>
-        }
-        <Route path="/login" render={() => (
-          <Login
-            handleLogin={this.handleLogin}
-          />
-        )} />
-        <Route path="/register" render={() => (
-          <Register
-            handleRegister={this.handleRegister}
-            errorText={this.state.errorText}
-          />
-        )}/>
+        ) : (
+          <nav>
+            <Link to="/register">
+              <button>Register</button>
+            </Link>
+            <Link to="/login">
+              <button>Login</button>
+            </Link>
+          </nav>
+        )}
+        <Route
+          path="/login"
+          render={() => <Login handleLogin={this.handleLogin} />}
+        />
+        <Route
+          path="/register"
+          render={() => (
+            <Register
+              handleRegister={this.handleRegister}
+              errorText={this.state.errorText}
+            />
+          )}
+        />
         <Navigation />
+        <RecipeGallery
+          // {...props}
+          currentUser={this.state.currentUser}
+          recipes={this.state.recipes}
+          // createRecipe={this.createRecipe}
+        />
         Hello world, please help
-    
       </div>
     );
   }
