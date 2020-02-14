1bea564b4a33c6cc2684b7de1e537b933bdcc696
Ash Rahman <asrahman97@gmail.com>
2020-02-14 15:03:14 -0500

    fixing some Local storage

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,9 @@ import {
   verifyUser,
   recipeSingle,
   recipesAll,
-  recipesCreate
+  recipesCreate,
+  recipeUpdate,
+  recipeDelete
 } from "./services/api_helper";
 import { Route, Link, withRouter } from "react-router-dom";
 import "./App.css";
@@ -15,6 +17,8 @@ import Navigation from "./components/navigation";
 import RecipeGallery from "./components/recipeGallery";
 import Recipe from "./components/singleRecipe";
 import Footer from "./components/footer";
+// import Welcome from 'react-welcome-page'
+
 
 class App extends Component {
   constructor(props) {
@@ -33,9 +37,10 @@ class App extends Component {
 
   handleRegister = async (e, registerData) => {
     e.preventDefault();
-    const currentUser = await registerUser(registerData);
+    const data = await registerUser(registerData);
     // if (!currentUser.errorMessage) {
-    // console.log(currentUser)
+    console.log(currentUser)
+    const currentUser = {user: data.user, id: data.id }
     this.setState({ currentUser });
     // this.props.history.push('/recipes');
     // } else {
@@ -48,8 +53,8 @@ class App extends Component {
     e.preventDefault();
     const currentUser = await loginUser(loginData);
     console.log(currentUser);
-    this.setState({ currentUser });
-    this.props.history.push("/");
+    this.setState({  currentUser });
+    // this.props.history.push("/");
   };
 
   handleLogout = () => {
@@ -83,14 +88,28 @@ class App extends Component {
     });
   };
 
+  updateRecipe = async (id, updates) => {
+    const newRecipe = await recipeUpdate(id, updates);
+    this.setState({
+      recipes: this.state.recipes.map(recipe => (
+        recipe.id === parseInt(id) ? newRecipe : recipe
+      )),
+      recipe: newRecipe
+    })
+  }
+
+  deleteRecipe = async (e, id) => {
+    e.preventDefault();
+    await recipeDelete(id);
+  }
   componentDidMount() {
     verifyUser();
     this.getRecipes();
-    console.log(localStorage.getItem('user'));
-    
+    console.log(localStorage.getItem("user"));
+
     if (localStorage.getItem("authToken")) {
       const user = localStorage.getItem("user");
-      const user1 = { user };
+      // const user1 = { user };
       user &&
         this.setState({
           currentUser: user
@@ -102,6 +121,43 @@ class App extends Component {
     console.log(this.state.currentUser);
     return (
       <div className="App">
+        {/* <Welcome
+          loopDuration={1000}
+          data={[
+            {
+              text: "Welcome",
+              imageAnimation: "fadeIn",
+              textAnimation: "fadeIn",
+              backgroundColor: "rgb(249, 216, 77)",
+              textColor: "#765907",
+              image: require("./assets/taco.png")
+            },
+            {
+              text: "To",
+              imageAnimation: "fadeIn",
+              textAnimation: "fadeIn",
+              backgroundColor: "rgb(228, 234, 236)",
+              textColor: "#37443B",
+              image: require("./assets/riceBall.png")
+            },
+            {
+              backgroundColor: "rgb(67, 167, 68)",
+              textColor: "#09311B",
+              text: "Recipe",
+              imageAnimation: "fadeIn",
+              textAnimation: "fadeIn",
+              image: require("./assets/leafyGreen.png")
+            },
+            {
+              text: "Serendipity",
+              image: require("./assets/pot-of-food.png"),
+              backgroundColor: "rgb(165, 43, 20)",
+              textColor: "#FDF0EE",
+              imageAnimation: "fadeIn",
+              textAnimation: "fadeIn"
+            }
+          ]}
+        /> */}
         {this.state.currentUser ? (
           <div>
             <h1>Hello, {this.state.currentUser.user}</h1>
