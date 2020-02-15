025b744197ae87c6665f7da041d06538b87adf29
Ash Rahman <asrahman97@gmail.com>
2020-02-15 15:49:36 -0500

    added review routes

--- recipe_rest/config/routes.rb
+++ recipe_rest/config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :recipes do
-    resources :ingredients
+    resources :reviews
   end
 
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
+
 
 
 end
