1823ce2668aacbf0fb92bb5700e2e36494305d54
Ash Rahman <asrahman97@gmail.com>
2020-02-19 18:23:55 -0500

    for css

--- client/src/components/reviews.js
+++ client/src/components/reviews.js
@@ -18,43 +18,54 @@ class Reviews extends Component {
 
   render() {
     console.log(this.props.reviews);
-    console.log(this.props.currentUser)
+    console.log(this.props.currentUser);
 
     return (
-      
       <div>
         {" "}
         {this.props.reviews && (
           <div className="reviews">
-            <h2>Reviews</h2>
-            
+            <h2 className="reviews-text">Reviews</h2>
+            {this.props.currentUser && (
+              <CreateReview
+                recipeId={this.props.recipeId}
+                createReview={this.props.createReview}
+                currentUser={this.props.currentUser}
+              />
+            )}
             {this.props.reviews.map((review, index) => {
               return (
-                <div key={index} className='review-box'>
+                <div key={index} className="review-box">
                   <p>{review.created_by}'s Review</p>
-                  <p><span className="bold">{review.score}:</span> {review.review_message}</p>
-                  {this.props.currentUser && this.props.currentUser.user === review.user &&
-                    <button className="delete" onClick={(e) => {
-                      e.preventDefault();
-                      let safeguard = window.confirm('You are about to delete this review! Press OK to confirm.');
-                      if (safeguard === true) {
-                        this.props.deleteReview(e, this.props.recipeId, review.id);
-                      }
-                    }
-                    }>
-                      Delete
-                  </button>}
+                  <p>
+                    <span className="bold">{review.score}:</span>{" "}
+                    {review.review_message}
+                  </p>
+                  {this.props.currentUser &&
+                    this.props.currentUser.user === review.user && (
+                      <button
+                        className="delete"
+                        onClick={e => {
+                          e.preventDefault();
+                          let safeguard = window.confirm(
+                            "You are about to delete this review! Press OK to confirm."
+                          );
+                          if (safeguard === true) {
+                            this.props.deleteReview(
+                              e,
+                              this.props.recipeId,
+                              review.id
+                            );
+                          }
+                        }}
+                      >
+                        Delete
+                      </button>
+                    )}
                 </div>
               );
             })}{" "}
           </div>
-        )}
-        {this.props.currentUser && (
-          <CreateReview
-            recipeId={this.props.recipeId}
-            createReview={this.props.createReview}
-            currentUser={this.props.currentUser}
-          />
         )}{" "}
       </div>
     );
