8eee9dba470f65f558c3eadb21c96908ea9dee86
Ash Rahman <asrahman97@gmail.com>
2020-02-19 10:52:58 -0500

    added another endpoint to target users

--- recipe_rest/app/controllers/users_controller.rb
+++ recipe_rest/app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 
   skip_before_action :authorize_request, only: :create
+  skip_before_action :authorize_request, only: :show
+
 
   # POST /signup
   # return authenticated token upon signup
@@ -10,6 +12,11 @@ class UsersController < ApplicationController
     auth_token = AuthenticateUser.new(user.user, user.password).call
     response = { message: Messages.account_created, auth_token: auth_token[0], user: auth_token[1], id: auth_token[2] }
     json_response(response, :created)
+  end
+
+  def show
+    user = User.find_by!(id: params[:id])
+    json_response(user)
   end
 
   private
